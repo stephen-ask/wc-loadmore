2fd945e4013c92f6a6c0a11923f494c56506a8fc
stephen-ask <stephenask22@gmail.com>
2023-02-24 16:48:52 +0530

    'check: product grid issue'

--- wc-loadmore.php
+++ wc-loadmore.php
@@ -73,7 +73,7 @@ class WC_Loadmore  {
 
         $args = array(
             'category' => array($category_id),
-            'posts_per_page' => 9,
+            'limit' => 9,
             'paged' => $paged
         );
 
