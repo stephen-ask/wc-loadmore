783a8b4ab3be396bced042a4c7514c5e5b5216ce
stephen-ask <stephenask22@gmail.com>
2023-02-24 12:43:35 +0530

    initial updates

---
+++ assets/js/wc-loadmore-script.js
@@ -0,0 +1,158 @@
+(function($) {
+    'use strict';
+    
+    $.noConflict( );
+    
+    var product_array = [];
+    var paged = 2;
+    
+    // HANDLES PRODUCTS AJAX REQUEST 
+    function ajax_post( url, data, paged ) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            success: function(response){
+                product_array = response.success == true ? response.data : [];
+                
+                if( paged == true && product_array.length == 0 ) {
+                    console.log('empty no products loaded');
+                    $('.product-load-more').html('NO MORE PRODUCTS TO LOAD');
+                    return;
+                }
+                $('.product-load-more').html('LOADMORE');                 
+                categoryProductlisting( product_array, paged );
+            },
+            processData: false,
+            contentType:false,
+        });
+    }
+    
+    // SETS PRODUCT GRID AFTER AJAX REQUEST
+    function categoryProductlisting( product_array, paged ) {
+        let productItem = [];
+
+        product_array.forEach((item, index)=>{
+            let rating_percentage = ( item.rating_count / 100 ) * 5;
+            productItem[index] = '<li class="blog-post product type-product post-'+item.product_id+' status-publish instock product_cat-seller has-post-thumbnail sale shipping-taxable purchasable product-type-simple">';
+                productItem[index] += '<a href="'+item.link+'"';
+                productItem[index] += 'class="woocommerce-LoopProduct-link woocommerce-loop-product__link">';
+                    productItem[index] += '<span class="onsale">Sale!</span>';
+                    
+                    // PRODUCT IMAGE 
+                    productItem[index] += '<img src="'+item.product_image+'" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail"';
+                    productItem[index] += 'alt=""  width="228" height="228">';
+                    
+                    // PRODUCT TITLE
+                    productItem[index] += '<h2 class="woocommerce-loop-product__title">'+item.product_name+'</h2>';
+                    
+                    // PRODUCT REVIEW
+                    productItem[index] += '<div class="eael-star-rating star-rating"><span style="width:'+rating_percentage+'%">';
+                        productItem[index] += 'Rated <strong class="rating">'+item.rating_count+'</strong> out of 5</span>';
+                    productItem[index] += '</div>';
+                    productItem[index] += '<span class="price">';
+                    
+                    // PRODUCT SALE PRICE
+                    if(item.sale_price != '') {
+                        productItem[index] += '<ins>';
+                            productItem[index] += '<span class="woocommerce-Price-amount amount"><bdi>';
+                            productItem[index] += '<span class="woocommerce-Price-currencySymbol">$</span>'+item.sale_price+'</bdi></span>';
+                        productItem[index] += '</ins>';
+                    }
+
+                    // PRODUCT PRICE 
+                    if(item.regular_price != '') {
+                        productItem[index] += '<del>';
+                            productItem[index] += '<span class="woocommerce-Price-amount amount">';
+                                productItem[index] += '<bdi>';
+                                    productItem[index] += '<span class="woocommerce-Price-currencySymbol">$</span>'+item.regular_price;
+                                productItem[index] += '</bdi>';
+                            productItem[index] += '</span>';
+                        productItem[index] += '</del>';
+                    }
+                    
+                    // VENDOR INFO
+                    if(item.vendor_info) {
+                        productItem[index] += '<div class="product-author-name"> By : '+item.vendor_info+'</div></span>';
+                    }
+
+                productItem[index] += '</a>';
+                
+                // WISHLIST 
+                productItem[index] +='<div class="yith-wcwl-add-to-wishlist add-to-wishlist-'+item.product_id+'  wishlist-fragment on-first-load" ';
+                productItem[index] +='data-fragment-ref="'+item.product_id+'" ';
+                productItem[index] +='data-fragment-options="';
+                productItem[index] +='{';
+                productItem[index] +='	&quot;base_url&quot;:&quot;&quot;,&quot;in_default_wishlist&quot;:false,&quot;is_single&quot;:false,&quot;';
+                productItem[index] +='	show_exists&quot;:false,&quot;product_id&quot;:'+item.product_id+',&quot;parent_product_id&quot;:'+item.product_id+',&quot;product_type&quot;';
+                productItem[index] +='	:&quot;simple&quot;,&quot;show_view&quot;:false,&quot;browse_wishlist_text&quot;:&quot;Browse wishlist&quot;';
+                productItem[index] +='	,&quot;already_in_wishslist_text&quot;:&quot;The product is already in your wishlist!&quot;,&quot;';
+                productItem[index] +='	product_added_text&quot;:&quot;Product added!&quot;,&quot;heading_icon&quot;:&quot;fa-heart-o&quot;,&quot;';
+                productItem[index] +='	available_multi_wishlist&quot;:false,&quot;disable_wishlist&quot;:false,&quot;show_count&quot;:false,&quot;';
+                productItem[index] +='	ajax_loading&quot;:false,&quot;loop_position&quot;:&quot;after_add_to_cart&quot;,&quot;item&quot;:&quot;';
+                productItem[index] +='	add_to_wishlist&quot;';
+                productItem[index] +='}">';
+                    productItem[index] +='	<div class="yith-wcwl-add-button">';
+                        productItem[index] +='<a href="?add_to_wishlist='+item.product_id+'&amp;_wpnonce=c7b9fa27a1" ';
+                        productItem[index] +='class="add_to_wishlist single_add_to_wishlist"';
+                        productItem[index] +='data-product-id="'+item.product_id+'" data-product-type="simple" ';
+                        productItem[index] +='data-original-product-id="'+item.product_id+'" ';
+                        productItem[index] +='data-title="Add to wishlist" ';
+                        productItem[index] +='rel="nofollow" >';
+                            productItem[index] +='<i class="yith-wcwl-icon fa fa-heart-o"></i><span>Add to wishlist</span>';
+                        productItem[index] +='	</a>';
+                    productItem[index] +='	</div>';
+                productItem[index] +='</div>';
+                    
+                // CART BTN
+                productItem[index] += '<a href="?add-to-cart='+item.product_id+'"';
+                productItem[index] += 'data-quantity="1"';
+                productItem[index] += 'class="button wp-element-button product_type_simple add_to_cart_button ajax_add_to_cart"';
+                productItem[index] += ' data-product_id="'+item.product_id+'" data-product_sku=""';
+                productItem[index] += 'aria-label="Add “'+item.product_name+'” to your cart" rel="nofollow">';
+                    productItem[index] += 'Add to cart';
+                productItem[index] += '</a>';
+
+            productItem[index] += '</li>';
+        });
+        
+        // CLEARS PRODUCTS ONLY IF CATEGORY OPTION SELECTED
+        if(paged == false) {
+            $('.products').empty();
+        }
+        
+        $('.products').append(productItem);
+        $('.woocommerce ul.products li.product, .woocommerce-page ul.products li.product').css('margin', '0 2.5% 2.2em 0');
+
+    }
+    $(document).ready(function(){
+        // LOADS PRODUCTS BASED ON CATEGORY 
+        $('.wc-product-category').click(function(e){
+            product_array=[];
+        
+            $('.wc-product-category').prop('checked', false);
+            $(this).prop('checked') ? $(this).prop('checked', false) : $(this).prop('checked', true);
+            let id = $(this).data('id');
+
+            let form = new FormData();
+            form.append( 'category_id', id );
+            form.append( 'action', 'product__list' );
+            ajax_post(ajaxurl, form, false);
+        });
+    });
+    
+    // LOADS PRODUCT PAGINATION 
+    $('.product-load-more').click(function(){
+        product_array=[];
+
+        let id = $('.wc-product-category').data('id');
+        let form = new FormData();
+        
+        form.append( 'category_id', id );
+        form.append( 'action', 'product__list' );
+        form.append( 'paged', paged );
+        
+        ajax_post(ajaxurl, form, true); 
+        paged++;
+    });
+})(jQuery);
---
+++ wc-loadmore.php
@@ -0,0 +1,148 @@
+<?php
+/*
+ * Plugin Name: Woocommerce Loadmore Plugin
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+class WC_Loadmore  {
+    // INITIALIZE VARIABLES
+    public $thumbnail_placeholder;
+
+    function __construct() {
+        $this->thumbnail_placeholder = 5921;
+        $this->init();
+    }
+    
+    // INIT PLUGIN NESSARY ELEMENTS
+    function init() {
+        // INIT THEME ACTIONS
+        add_action( 'wp_enqueue_scripts', [$this, 'load_styles'], PHP_INT_MAX );
+        add_action( 'wp_enqueue_scripts', [$this,'load_scripts'], PHP_INT_MAX );
+        add_action( 'plugins_loaded', [$this,'init_woocommerce'] );
+        
+        // INIT THEME SHORTCODES 
+        add_shortcode( 'category_listing', [$this,'product_category_list'] );
+    }
+    
+    function init_woocommerce() {
+        add_action( 'wp_ajax_priv_product__list', [$this,'product__list'] );
+        add_action( 'wp_ajax_nopriv_product__list', [$this,'product__list'] );
+        
+    
+        add_filter( 'woocommerce_shortcode_products_query', [$this,'add_custom_button_to_product_shortcode'] );
+        add_action( 'woocommerce_after_shop_loop_item',  [$this,'woocommerce_custom_button'], 5 );
+        add_action( 'woocommerce_shortcode_after_products_loop',  [$this,'loadmore_btn'], 11 );
+    }
+
+    function add_custom_button_to_product_shortcode( $atts ) {
+        $atts['custom_button'] = true;
+        $atts['loadmore'] = true;
+        return $atts;
+    }
+    
+    // PLUGIN STYLE CALLBACK
+    function load_styles() {
+        wp_enqueue_style( 'wc-loadmore-style', plugin_dir_url(__FILE__). 'assets/css/wc-loadmore-style.css' );
+    }
+    
+    // PLUGIN JS SCRIPTS CALLBACK
+    function load_scripts() {
+        wp_enqueue_script( 'wc-loadmore-script', plugin_dir_url(__FILE__).'assets/js/wc-loadmore-script.js', array(), '0.1', true );
+    }
+
+    // WOOCOMMERCE WISHLIST BTN FOR PRODUCTS
+    function woocommerce_custom_button() {
+        echo do_shortcode( '[yith_wcwl_add_to_wishlist]' );
+    }
+    
+    // LOADMORE BUTTON CALLBACK
+    function loadmore_btn() {
+        echo '<button class="button product-load-more">LOADMORE</button>';
+    }
+
+    // PRODUCTS LISTING WITH PAGINATION 
+    function product__list(){
+        
+        // PARAMETERS
+        $category_id = sanitize_text_field( $_POST['category_id'] );
+        $paged = sanitize_text_field( $_POST['paged'] ) ?? 1;
+        $products_array = array();
+
+        $args = array(
+            'category' => array($category_id),
+            'paged' => $paged
+        );
+
+        // RETURN ERROR IF FUNTION DOES NOT EXIST 
+        if( !function_exists( "wc_get_products" ) ) wp_send_json_error("wc_get_products was not exist");
+        
+        $product_list = wc_get_products($args);
+        
+        
+        // RETURN MSG IF PRODUCT EMPTY
+        if(empty($product_list)) return wp_send_json_error("No Products Found");
+
+        // FETCH NESSORY PRODUCT INFO
+        foreach($product_list as $product) {
+            $product_id = $product->get_id();
+            $vendor_id = get_post_field( 'post_author', $product_id );
+            $vendor = get_userdata( $vendor_id );
+            $product_image = get_post_thumbnail_id( $product_id ) ?? get_post_thumbnail_id( $this->thumbnail_placeholder );
+            $products_array[] = array(
+                'product_id' => $product_id,
+                'product_name' => $product->get_name(),
+                'product_title' => $product->get_title(),
+                'product_status' => $product->get_status(),
+                'price' => $product->get_price(),
+                'sale_price' => $product->get_sale_price(),
+                'regular_price' => $product->get_regular_price(),
+                'product_image' => wp_get_attachment_image_src( $product_image, 'single-post-thumbnail' )[0],
+                'rating_count' => $product->get_average_rating(),
+                'review_count' => $product->get_review_count(),
+                'vendor_info' => $vendor->display_name,
+                'link' => get_permalink($product_id)
+            );
+        }
+        
+        // RETURN DATA
+        wp_send_json_success($products_array);
+    }
+
+    function product_category_list($attr) {
+        
+        // GET PRODUCT CATEGORIES 
+        $terms = get_terms(
+            array(
+                "cat" => "product_cat"
+            )
+        );
+        
+        // RETURN MSG IF CATEGORY IS EMPTY
+        if(empty($terms)) $html = "No Records Found"; 
+
+        $html = "<div class='wc-product-categories-container'>";
+            $html .= "<ul class='wc-product-categories'>";
+            $i = 1;
+            foreach($terms as $term) {
+                $html .= "<li class='item'>";
+                    $html .= "<label for='wc-product-category-$i'>";
+                    $html .= "<input id='wc-product-category-$i' type='checkbox' name='wc-product-category' class='wc-product-category' value='$term->name' data-id='$term->slug'>";
+                        $html .=  $term->name;
+                    $html .= "</label>";
+                    
+                $html .= "</li>";
+                $i++;
+            }
+            $html .= "</ul>";
+        
+        // RETURN CATEGORIES LIST
+        return $html .= "</div>";   
+    }
+}
+
+new WC_Loadmore();
+
+
